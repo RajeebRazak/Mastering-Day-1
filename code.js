88efda208cd3b656e7f1afc9982665ccd1b14042
RajeebRazak <145429667+RajeebRazak@users.noreply.github.com>
2023-09-23 15:22:25 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,29 @@
+var name = "RAJEEBRAZAK";
+var batch ="B52WD Tamil";
+var placed = "he is placed at ZOHO";
+console.log(name)
+
+var name = "RAJEEBRAZAK";
+var batch = "B52WD Tamil";
+var placed = "he is placed at ZOHO";
+console.log(batch);
+
+var name = "RAJEEBRAZAK";
+var batch = "B52WD Tamil";
+var placed = "he is placed at ZOHO";
+console.log(placed);
+
+var age = 15;
+var ageString = age.toString();
+console.log(ageString);
+
+var age = 15;
+var ageString = age.toString();
+console.log(ageString);
+
+var age = 40;
+var ageString=age.toString();
+console.log(ageString);
+
+var fruits = "apple"
+console.log(fruits);
